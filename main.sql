6ec6969e3685487d93d2ed88c8e6137b8301e57d
Hardeep Sidhu <hss70@bath.ac.uk>
2025-01-20 20:38:56 +0000

    Added the initial data and SQL script

---
+++ main.sql
@@ -0,0 +1,46 @@
+
+
+CREATE TABLE Item (
+	  ItemName VARCHAR (30) NOT NULL,
+  ItemType CHAR(1) NOT NULL,
+  ItemColour VARCHAR(10),
+  PRIMARY KEY (ItemName));
+
+CREATE TABLE Employee (
+  EmployeeNumber SMALLINT UNSIGNED NOT NULL ,
+  EmployeeName VARCHAR(10) NOT NULL ,
+  EmployeeSalary INTEGER UNSIGNED NOT NULL ,
+  DepartmentName VARCHAR(10) NOT NULL REFERENCES Department,
+  BossNumber SMALLINT UNSIGNED NOT NULL REFERENCES Employee,
+  PRIMARY KEY (EmployeeNumber));
+
+CREATE TABLE Department (
+  DepartmentName VARCHAR(10) NOT NULL,
+  DepartmentFloor SMALLINT UNSIGNED NOT NULL,
+  DepartmentPhone SMALLINT UNSIGNED NOT NULL,
+  EmployeeNumber SMALLINT UNSIGNED NOT NULL REFERENCES 
+    Employee,
+  PRIMARY KEY (DepartmentName));
+
+CREATE TABLE Sale (
+  SaleNumber INTEGER UNSIGNED NOT NULL,
+  SaleQuantity SMALLINT UNSIGNED NOT NULL DEFAULT 1,
+  ItemName VARCHAR(30) NOT NULL REFERENCES Item,
+  DepartmentName VARCHAR(10) NOT NULL REFERENCES Department,
+  PRIMARY KEY (SaleNumber));
+
+CREATE TABLE Supplier (
+  SupplierNumber INTEGER UNSIGNED NOT NULL,
+  SupplierName VARCHAR(30) NOT NULL,
+  PRIMARY KEY (SupplierNumber));
+
+CREATE TABLE Delivery (
+  DeliveryNumber INTEGER UNSIGNED NOT NULL,
+  DeliveryQuantity SMALLINT UNSIGNED NOT NULL DEFAULT 1,
+  ItemName VARCHAR(30) NOT NULL REFERENCES Item,
+  DepartmentName VARCHAR(10) NOT NULL REFERENCES Department,
+  SupplierNumber INTEGER UNSIGNED NOT NULL REFERENCES  
+     Supplier,
+  PRIMARY KEY (DeliveryNumber));
+
+-- using the data in the text files, insert into the tables this information
